6f286a95f46157249fc7cd0e0edf85cd6eab4af6
fareedmirzay <fareedmirzay@ymail.com>
2024-08-30 14:05:18 -0400

    exports lab completed with many successful test

--- Working-with-External-Data/index1.js
+++ Working-with-External-Data/index1.js
@@ -210,7 +210,64 @@ function updateProgress(progressEvent) {
 
 
 // /**
-//  * 9. Test your favourite() function by creating a getFavourites() function.
+//  * 7. As a final element of progress indication, add the following to your axios interceptors:
+//  * - In your request interceptor, set the body element's cursor style to "progress."
+//  * - In your response interceptor, remove the progress cursor style from the body element.
+//  */
+// /**
+//  * 8. To practice posting data, we'll create a system to "favourite" certain images.
+//  * - The skeleton of this function has already been created for you.
+//  * - This function is used within Carousel.js to add the event listener as items are created.
+//  *  - This is why we use the export keyword for this function.
+//  * - Post to the cat API's favourites endpoint with the given ID.
+//  * - The API documentation gives examples of this functionality using fetch(); use Axios!
+//  * - Add additional logic to this function such that if the image is already favourited,
+//  *   you delete that favourite using the API, giving this function "toggle" functionality.
+//  * - You can call this function by clicking on the heart at the top right of any image.
+//  */
+// export async function favourite(imgId) {
+//   // your code here
+// }
+
+
+
+
+
+export async function favourite(imgId) {
+    axios.get("https://api.thecatapi.com/v1/favourites")
+    .then((res) =>
+      res.data.forEach((i) =>
+        axios.delete(`https://api.thecatapi.com/v1/favourites/${i.id}`),
+      ),
+    );
+
+    // GET all favourites
+    axios.get("https://api.thecatapi.com/v1/favourites").then((res) => {
+        console.log("FAVS => ", res.data);
+    
+        let deleted;
+    
+        // loop over the items
+        res.data.forEach((item) => {
+          // if the image is favourited then delete
+          if (item.image_id === imgId) {
+            // delete
+            deleted = true;
+            axios.delete(`https://api.thecatapi.com/v1/favourites/${item.id}`);
+          }
+        });
+    
+        if (!deleted) {
+          // add
+          axios
+            .post("https://api.thecatapi.com/v1/favourites", { image_id: imgId })
+            .then((res) => console.log(res.data));
+        }
+      });
+    }
+
+
+    //  * 9. Test your favourite() function by creating a getFavourites() function.
 //  * - Use Axios to get all of your favourites from the cat API.
 //  * - Clear the carousel and display your favourites when the button is clicked.
 //  *  - You will have to bind this event listener to getFavouritesBtn yourself.
@@ -219,30 +276,32 @@ function updateProgress(progressEvent) {
 //  *    repeat yourself in this section.
 //  */
 
+// /**
 
-export async function favourite(imgId) {
-    axios.post('https://api.thecatapi.com/v1/favourites', {"image_id": imgId})
-    .then(res => console.log(res))
-}
+    getFavouritesBtn.addEventListener('click', getFavourites);
 
-    // GET all favourites
-  axios.get("https://api.thecatapi.com/v1/favourites").then((res) => {
-    console.log("FAVS => ", res.data);
+    function getFavourites() {
+        axios.get("https://api.thecatapi.com/v1/favourites")
+        .then(res => {
+            console.log('All FAVS::: ', res.data);
+            Carousel.clear();
+    
+            res.data.forEach(item => {
+                const element = Carousel.createCarouselItem(item.image.url, item.image_id, item.image.id)
+    
+                Carousel.appendCarousel(element);
+            });
+        })
+        Carousel.start();
+    }
 
-    // loop over the items
-    res.data.forEach((item) => {
-        
-        // if the image is favourited then delete
-      if (item.image_id === imgId) {
-        // delete
-        axios.delete(`https://api.thecatapi.com/v1/favourites/${item.id}`);
 
-    // Else add to favourites
-    } else {
-        // add
-        axios
-          .post("https://api.thecatapi.com/v1/favourites", { image_id: imgId })
-          .then((res) => console.log(res.data));
-      }
-    });
-  });
+
+
+// /**
+//  * 10. Test your site, thoroughly!
+//  * - What happens when you try to load the Malayan breed?
+//  *  - If this is working, good job! If not, look for the reason why and fix it!
+//  * - Test other breeds as well. Not every breed has the same data available, so
+//  *   your code should account for this.
+//  */
